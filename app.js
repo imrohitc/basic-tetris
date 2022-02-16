77aaf14b5ce8ce5e55547affd4adcca13da47574
Rohit Chavan <rjc0160@gmail.com>
2022-02-17 01:06:14 +0530

    draw tetrominoes

--- app.js
+++ app.js
@@ -11,33 +11,57 @@ document.addEventListener('DOMContentLoaded', () => {
         [width, width+1, width+2, width*2+2],
         [1, width+1, width*2, width*2+1],
         [width, width*2, width*2+1, width*2+2]
-    ]
+    ];
 
     const zTetromino = [
         [width+1, width+2, width*2, width*2+1],
         [0, width, width+1, width*2+1],
         [width+1, width+2, width*2, width*2+1],
         [0, width, width+1, width*2+1]
-    ]
+    ];
 
     const tTetromino = [
         [1, width, width+1, width+2],
         [1, width+1, width+2, width*2+1],
         [width, width+1, width+2, width*2+1],
         [1, width, width+1, width*2+1]
-    ]
+    ];
 
     const oTetromino = [
         [0, 1, width, width+1],
         [0, 1, width, width+1],
         [0, 1, width, width+1],
         [0, 1, width, width+1]
-    ]
+    ];
 
     const iTetromino = [
         [1, width+1, width*2+1, width*3+1],
         [width, width+1, width+2, width+3],
         [1, width+1, width*2+1, width*3+1],
         [width, width+1, width+2, width+3]
-    ]
+    ];
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+
+    //Randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random() * theTetrominoes.length);
+    let current = theTetrominoes[random][currentRotation];
+
+    //Draw the Tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add("tetromino");
+        });
+    }
+
+    //Undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove("Tetromino");
+        });
+    }
+    
 })
