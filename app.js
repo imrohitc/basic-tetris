8c3a84a29ce6023adb515ae52e23d9efe93d9edf
Rohit Chavan <rjc0160@gmail.com>
2022-02-20 01:43:32 +0530

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.querySelector("#score");
     const startBtn = document.querySelector("#start-button");
     const width = 10;
+    let nextRandom = 0;
 
     //The Tetrominoes
     const lTetromino = [
@@ -96,10 +97,13 @@ document.addEventListener('DOMContentLoaded', () => {
             current.forEach(index => squares[currentPosition + index].classList.add("taken"));
 
             //Start a new Tetromino falling
-            random = Math.floor(Math.random() * theTetrominoes.length);
+            random = nextRandom;
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length);
             current = theTetrominoes[random][currentRotation];
             currentPosition = 4;
             draw();
+            
+            displayShape();
         }
     }
 
@@ -142,5 +146,31 @@ document.addEventListener('DOMContentLoaded', () => {
         current = theTetrominoes[random][currentRotation];
 
         draw();
+    }
+
+    //Show up-next Tetromino in mini-grid
+    const displaySquares = document.querySelectorAll(".mini-grid div");
+    const displayWidth = 4;
+    let displayIndex = 0;
+
+    //The Tetrominoes without rotations
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+        [displayWidth+1, displayWidth+2, displayWidth*2, displayWidth*2+1], //zTetromino
+        [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //oTetromino
+        [1, displayWidth+1, display*2+1, display*3+1]  //iTetromino
+    ]
+
+    //Display the shape in the mini-grid display
+    function displayShape() {
+        //Remove any trace of a tetromino from the entire grid
+        displaySquares.forEach(square => {
+            square.classList.remove("tetromino");
+        })
+
+        upNextTetrominoes[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add("tetromino");
+        });
     }
 })
