b32069d893cf107b92f75a74a71fc62e9f46f2a3
Rohit Chavan <rjc0160@gmail.com>
2022-02-20 13:52:53 +0530

    add addScore and gameOver functions, colors to tetrominoes

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector("#start-button");
     const width = 10;
     let nextRandom = 0;
+    let timerId;
+    let score = 0;
+    const colors = [
+        "orange",
+        "red",
+        "purple",
+        "green",
+        "blue"
+    ];
 
     //The Tetrominoes
     const lTetromino = [
@@ -55,6 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add("tetromino");
+            squares[currentPosition + index].style.backgroundColor = colors[random];
         });
     }
 
@@ -62,11 +72,9 @@ document.addEventListener('DOMContentLoaded', () => {
     function undraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove("tetromino");
+            squares[currentPosition + index].style.backgroundColor = "";
         });
     }
-
-    //Make the tetromino move down every second
-    timeID = setInterval(moveDown, 1000);
 
     //Assign functions to keyCodes
     function control(e) {
@@ -89,6 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition += width;
         draw();
         freeze();
+        gameOver();
     }
 
     //Freeze function
@@ -102,8 +111,8 @@ document.addEventListener('DOMContentLoaded', () => {
             current = theTetrominoes[random][currentRotation];
             currentPosition = 4;
             draw();
-
             displayShape();
+            addScore();
         }
     }
 
@@ -151,7 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //Show up-next Tetromino in mini-grid
     const displaySquares = document.querySelectorAll(".mini-grid div");
     const displayWidth = 4;
-    let displayIndex = 0;
+    const displayIndex = 0;
 
     //The Tetrominoes without rotations
     const upNextTetrominoes = [
@@ -167,10 +176,53 @@ document.addEventListener('DOMContentLoaded', () => {
         //Remove any trace of a tetromino from the entire grid
         displaySquares.forEach(square => {
             square.classList.remove("tetromino");
+            square.style.backgroundColor = "";
         })
 
         upNextTetrominoes[nextRandom].forEach(index => {
             displaySquares[displayIndex + index].classList.add("tetromino");
+            displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom];
         });
+    }
+
+    //Add functionality to the button
+    startBtn.addEventListener("click", () => {
+        if(timerId) {
+            clearInterval(timerId);
+            timerId = null;
+        } else {
+            draw();
+            timerId = setInterval(moveDown, 1000);
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+            displayShape();
+        }
+    })
+
+    //Add score
+    function addScore() {
+        for(let i=0; i<199; i+=width) {
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
+
+            if(row.every(index => squares[index].classList.contains("taken"))) {
+                score += 10;
+                scoreDisplay.innerHTML = score;
+                row.forEach(index => {
+                    squares[index].classList.remove("taken");
+                    squares[index].classList.remove("tetromino");
+                    squares[index].style.backgroundColor = "";
+                })
+                const squaresRemoved = squares.splice(i, width);
+                squares = squaresRemoved.concat(squares);
+                squares.forEach(cell => grid.appendChild(cell));
+            }
+        }
+    }
+
+    //Game over
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains("taken"))) {
+            scoreDisplay.innerHTML = "end";
+            clearInterval(timerId);
+        }
     }
 })
